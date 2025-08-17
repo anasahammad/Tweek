b2212df30f9d9f2f78e0af908b436639f8a52d32
anasahammad <anasahammad2002@gmail.com>
2025-08-17 16:53:35 +0600

    feat: add PostComposer component and useCreatePost hook for creating posts with image support

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,16 +1,29 @@
 
+import PostComposer from '@/components/PostComposer';
 import SignOutButton from '@/components/SignOutButton';
 import { useUserSync } from '@/hooks/useUserSync';
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
     useUserSync()
     return (
         <SafeAreaView className='flex-1 bg-white'>
-            <Text>Home Screen</Text>
-            <SignOutButton/>
+             <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
+        <Ionicons name="logo-twitter" size={24} color="#1DA1F2" />
+        <Text className="text-xl font-bold text-gray-900">Home</Text>
+        <SignOutButton />
+      </View>
+
+      <ScrollView 
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{paddingBottom:80}}
+      className='flex-1'
+      >
+<PostComposer />
+      </ScrollView>
         </SafeAreaView>
     );
 };
